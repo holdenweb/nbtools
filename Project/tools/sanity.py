08d661b2b55b85438afde2c8059a9fc4dbf2e14d
Steve Holden <steve@holdenweb.com>
2014-03-02 23:52:41 -0800

    Build notebooks from sources, reporting on missing sources. This is as far as templating goes, much of the rest of the work depends on parsing notebooks and actioning the pragmas they contain. This requires more design work, but I think we may already be at a stage where the system will do much useful work.
    Now we need to polish this branch up (a little), commit and
    merge back into the master branch as an early release.

--- Project/tools/sanity.py
+++ Project/tools/sanity.py
@@ -1,7 +1,10 @@
 """
 sanity.py: Ensure the project structure's integrity is uncompromised.
 """
+import datetime
 import os
+
+import IPython.nbformat.current as nbf
 
 from jinja2 import Environment, FileSystemLoader
 
@@ -9,7 +12,12 @@ def nullstrip(file):
     for line in file:
         if line.strip() and line[0] != "#":
             yield line
-    
+
+def newer(file1, file2):
+    file1_creation = os.stat(file1).st_ctime
+    file2_creation = os.stat(files).st_ctime
+    return file1_creation > file2_creation
+
 # Load configuration data from outline (currently just a list of topics)
 names = [line.strip().rstrip(" *")
          for line in nullstrip(open("outline.txt", "r"))]
@@ -32,18 +40,42 @@ root = Directory(".")
 jenv = Environment(loader=FileSystemLoader("data/templates"))
 nb_template = jenv.get_template("base.ipynb")
 
-# Check all required notebooks exist
+# Check all required notebook sources exist
+# (and regenerate them if their source notebook
+# is newer than the target - make is on the horizon
 for tag in tags:
-    filename = os.path.join("notebooks", tag+".ipynb")
-    if not os.access(filename, os.R_OK):
+    now = datetime.datetime.today()
+    nb_name = tag+".ipynb"
+    src_file = os.path.join("nbsource", nb_name)
+    dest_file = os.path.join("notebooks", nb_name)
+    try:
+    except FileNotFoundError:
+        print ("Missing source:", src_file)
+        continue
+    # The cells in the template are copied across
+    # unless they contain processing instructions.
+    # We currently assume that none do, and treat
+    # and empty cell as needing to be replaced with
+    # the cells from the source notebook.
+    # Ultimately this will be handled by pragmas.
+    if not (os.access(src_file, os.R_OK) or
+            newer(src_file, dest_file)):
         # Publication notebooks (which is what I had
         # in mind) will soon be automated by make,
         # as will other notebook types. All driven
         # by a scrupulously combed source. I hope.
-        new_nb = open(filename, "w")
-        nb_content = nb_template.render({"title": tag_names[tag],
-                                         "date": "today",
-                                         "time": "now"}) 
+        # For now, let's just be happy with what we have.
+        new_nb = open(dest_file, "w")
+        nb_content = nb_template.render({"slug": tag,
+                                         "title": tag_names[tag],
+                                         "date": now.date(),
+                                         "time": now.time(),
+                                         "src_file": src_file}) 
         new_nb.write(nb_content)
         new_nb.close()
-        print("Created missing notebook", tag)
+        # Now we've done the easy stuff (templating)
+        # we read in the newly-created notebook and
+        # manipulate its structure according to the
+        # pragmas found in the various cells.
+        
+        print("Created notebook", tag)
