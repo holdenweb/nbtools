99f2e5f73cdefa27f61b68c1f2915976dd08e15f
Steve Holden <steve@holdenweb.com>
2014-03-14 12:35:25 -0700

    Add script to do nightly content updates to notebooks project.

---
+++ project/tools/nightly.sh
@@ -0,0 +1,27 @@
+export PROJECT_HOME=~/Projects/Python/intVideos/project
+NBSTATS=$PROJECT_HOME/tools/nbstats.py
+TOPICS=$PROJECT_HOME/tools/topics.py
+SANITY=$PROJECT_HOME/tools/sanity.py
+export CONTENT_HOME=~/Projects/Python/intermediate-notebooks
+
+echo "Copying outline"
+cd $PROJECT_HOME
+cp outline.txt $CONTENT_HOME
+echo "Creating statistics"
+echo "   Content Analysis"
+(cd $PROJECT_HOME/nbsource;
+    $NBSTATS *.ipynb > $CONTENT_HOME/nbstats.txt)
+echo "   Unstarted topics"
+$TOPICS -u > $CONTENT_HOME/unstarted.txt
+echo "   Orphaned notebooks"
+$TOPICS -o > $CONTENT_HOME/orphaned.txt
+echo "Generating and transferring notebooks"
+rm notebooks/*.ipynb
+$SANITY
+tar cfh - ./notebooks/*.ipynb ./notebooks/nb | (cd $CONTENT_HOME; tar xf -)
+echo "---------------------------"
+(cd $CONTENT_HOME;
+    git add *
+    git status
+    git commit -m "Nightly dump for `date `"
+)
