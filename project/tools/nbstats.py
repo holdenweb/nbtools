36e01534bc4502b6f1b814edd9bebd0d80204d01
Steve Holden <steve@holdenweb.com>
2014-03-15 19:11:12 -0700

    Major refactoring to allow both directories and filenames as arguments.

--- project/tools/nbstats.py
+++ project/tools/nbstats.py
@@ -18,40 +18,44 @@ import IPython.nbformat.current as nbf
 if len(sys.argv) > 1:
     paths = sys.argv[1:]
 else:
-    paths = glob("nbsource/*.ipynb")
+    paths = glob("*.ipynb")
 
 results = []
 for path in paths:
-    notebook = nbf.read(open(path), "ipynb")
-    filename = os.path.basename(path)
-    code_lines = code_bytes = doc_lines = doc_bytes = 0
-    worksheet = notebook.worksheets[0]
-    cell_count = len(worksheet.cells)
-    #print(path, cell_count)
-    cell_type_count = defaultdict(int)
-    if cell_count > 1:
-        for cell in worksheet.cells:
-            cell_type = cell.cell_type
-            cell_type_count[cell_type] += 1
-            if cell_type == "heading":
-                cell_type = "heading"+str(cell["level"])
-            if cell_type == "markdown":
-                source = cell.source.splitlines()
-                lines = sum(l != "\n" for l in source)
-                bytes = sum(len(l) for l in source)
-                doc_lines += lines
-                doc_bytes += bytes
-            elif cell_type == "code":
-                source = cell.input.splitlines()
-                lines = sum(l != "\n" for l in source)
-                bytes = sum(len(l) for l in source)
-                code_lines += lines
-                code_bytes += bytes
-        results.append((path, cell_type_count["code"], code_lines,
-                        code_bytes, cell_type_count["markdown"],
-                        doc_lines, doc_bytes))
+    if os.path.isdir(path):
+        files = glob(os.path.join(path, "*.ipynb"))
+    else:
+        files = [path]
+    for in_file in files:
+        notebook = nbf.read(open(in_file), "ipynb")
+        filename = os.path.basename(in_file)
+        code_lines = code_bytes = doc_lines = doc_bytes = 0
+        worksheet = notebook.worksheets[0]
+        cell_count = len(worksheet.cells)
+        cell_type_count = defaultdict(int)
+        if cell_count > 1:
+            for cell in worksheet.cells:
+                cell_type = cell.cell_type
+                cell_type_count[cell_type] += 1
+                if cell_type == "heading":
+                    cell_type = "heading"+str(cell["level"])
+                if cell_type == "markdown":
+                    source = cell.source.splitlines()
+                    lines = sum(l != "\n" for l in source)
+                    bytes = sum(len(l) for l in source)
+                    doc_lines += lines
+                    doc_bytes += bytes
+                elif cell_type == "code":
+                    source = cell.input.splitlines()
+                    lines = sum(l != "\n" for l in source)
+                    bytes = sum(len(l) for l in source)
+                    code_lines += lines
+                    code_bytes += bytes
+            results.append((in_file, cell_type_count["code"], code_lines,
+                            code_bytes, cell_type_count["markdown"],
+                            doc_lines, doc_bytes))
 if results:
-    longest = max(len(r[0]) for r in results)
+    longest = max(len(r[0]) for r in results)-6 # = len(".ipynb")
 else:
     sys.exit("No files with more than one cell")
 
@@ -61,6 +65,8 @@ format_hdr = "{:>{length}} {}"
 print(format_hdr.format("", "------ code ------ ---- markdown ----", length=longest))
 print(format_hdr.format("", "cells  lines chars cells  lines chars", length=longest))
 for result in results:
+    result = list(result)
+    result[0] = result[0][:-6]
     totals = [total+count for (total, count) in zip(totals, result[1:])]
     print(format_str.
           format(*result, length=longest))
