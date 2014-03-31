f1a8a09f8bfcdf9ac8732e8131b1bf7fff5aaf99
Rachel Kelly <rachelkelly@gmail.com>
2014-03-31 16:09:40 -0700

    first hopeful candidate

--- project/tools/mdstrip.py
+++ project/tools/mdstrip.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # mdstrip.py: makes new notebook from old, stripping md out
 
 """A tool to copy cell_type=("code") into a new file
@@ -14,8 +16,6 @@ from lib import get_project_dir, slugify
 from shutil import copyfileobj # to copy pieces
 from mktopic import mktopic # to make new nb
 
-worksheet = notebook.worksheets[0] # this should probably go elsewhere
-
 def mdstrip(paths):
 	for path in paths:
 		if os.path.isdir(path):
@@ -24,31 +24,23 @@ def mdstrip(paths):
             files = [path]
         for in_file in files:
            	input_nb = nbf(open(in_file), "ipynb")
+           	worksheet = input_nb.worksheets[0]
            	print "the file you're working with is: %s" % input_nb
-        	imported_nb = basename(in_file) # will this work since we set var to entire
+        	input_nb_name = basename(in_file) # will this work since we set var to entire
         								 # filename, not file.ipynb?
             # read CELLS rather than lines - will be same?
+            # first to make new notebook
+            # read nb & make list of all code cells
+            cell_list = []
             for cell in worksheet.cells:
             	# cell counting mechanism here to tell how many are ported into new .ipynb file?
-                   	if cell_type in ("code"):
-                   		# first to make new notebook via base ipynb template
+                   	if cell_type == ("code"):
+               			cell_list.append(cell)
+
+                   		# first to make new notebook
                    		# then to write every cell that meets cell_type=("code") to new nb
                    		# should this be a list of all relevant cells?
-                   		# # # ALLLLL this stuff: src_template,now,dst_file,
-                   		# # # & render_context is just to make a new nb
-                   		# 
-                   		# also are these labels relevant when importing them?  title slug etc?
-            			src_template = template_env.get_template("base.ipynb") 
-            			# not sure if is steve's base or ipython's
-						now = datetime.datetime.today()
-				    	new_nb = open(imported_nb+"-minus-md.ipynb", 'w+') #e.g. tuples-minus-mid.ipynb
-				    	dst_file = os.path.join("mdless_nbs", new_nb) # think this just puts it in dir
-				    	render_context = {"slug": slug, # no longer have slug, just new_nb
-                    		"title": title, # how will this be generated
-                    		"date": now.date(),
-                    		"time": now.time(),
-                    		"src_file": new_nb,
-                    		"template": src_template.filename} # stevenote: we are writing a source ...
+
             			# now copy contents equalling
         				# source_content = txt_file.read()
         				# dst_file.write(source_content)
@@ -56,9 +48,11 @@ def mdstrip(paths):
     				else:
     					break # do nothing for header/markdown
     						  # note: may want to do something for single md cell with logo & name.
-    if os.path.isfile(dst_file):
-        # If the topic exists do not overwrite it XXX [unless option set].
-        sys.exit("file {} already exists".format(dst_file))
+	  		output_nb = nbf.new_notebook()
+	  		output_nb_name = os.path.join("code_nbs", input_nb_name)
+	  		output_nb.worksheets.append(nbf.new_worksheet(cells=cell_list))
+	  		with open(output_nb_name, 'w') as f:
+	  			nbf.write(output_nb, f, "ipynb")
 
 if __name__ == "__main__":
     os.chdir(os.path.join(get_project_dir(), "nbsource"))
