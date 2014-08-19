61c9289f3d252f4f027a1cfc7342c50bda80ffd6
Steve Holden <steve@holdenweb.com>
2014-08-19 09:36:12 -0700

    Store notebooks with standard indentation and sorted keys.

--- project/tools/normalize.py
+++ project/tools/normalize.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
 
-# mdstrip.py: makes new notebook from old, stripping md out
+# normalize.py: makes new notebook from old, stripping out code cell
+#               numbering and output cells
 
 """A tool to copy cell_type=("code") into a new file
 without grabbing headers/markdown (most importantly the md)
 NOTE: may want to grab the headers after all, or define new ones?"""
 
-import os
-import IPython.nbformat.current as nbf
 from glob import glob
-from lib import get_project_dir
+import io
+import json
+import os
 import sys
 
+import IPython.nbformat.current as nbf
 
+from lib import get_project_dir
 
 def normalize(in_file, out_file):
     worksheet = in_file.worksheets[0]
@@ -26,7 +29,12 @@ def normalize(in_file, out_file):
         cell_list.append(cell)
     output_nb = nbf.new_notebook() # XXX should set name ...
     output_nb.worksheets.append(nbf.new_worksheet(cells=cell_list))
-    nbf.write(output_nb, out_file, "ipynb")
+    tmp_file = io.StringIO()
+    nbf.write(output_nb, tmp_file, "ipynb")
+    # Then write reorganized (i.e. key-sorted) JSON file to out_file
+    tmp_file.seek(0)
+    j_nb = json.load(tmp_file)
+    json.dump(j_nb, out_file, sort_keys=True, indent=2)
 
 
 if __name__ == "__main__":
@@ -41,4 +49,5 @@ if __name__ == "__main__":
     try:
         normalize(nbf.read(infile, "ipynb"), outfile)
     except Exception as e:
-        sys.exit("Normalization error: '{}'".format(str(e)))
+        print("Normalization error: '{}'".format(str(e)))
+        sys.exit(-1)
