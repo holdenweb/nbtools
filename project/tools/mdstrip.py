5db87031c0152a8280542dc0fd2a56700d519efa
Steve Holden <steve@holdenweb.com>
2014-09-08 08:50:12 -0700

    Modify mdstrip for Portia's classes

--- project/tools/mdstrip.py
+++ project/tools/mdstrip.py
@@ -8,21 +8,17 @@ NOTE: may want to grab the headers after all, or define new ones?"""
 
 import os
 import IPython.nbformat.current as nbf
-from os.path import basename # to strip off file extension before re-adding it :)
+from os.path import basename
 from glob import glob
 from lib import get_project_dir, nullstrip, slugify
 import sys
+import io
 
+tools_dir = os.path.dirname(os.path.realpath(__file__))
+title_data = open(os.path.join(tools_dir, "TitleBook.ipynb")).read()
 
 def mdstrip(paths):
     for path in paths:
-
-        outline = nullstrip(open("outline.txt"))
-        slug_dict = {}
-        for line in outline:
-            title = line.strip().rstrip(" *")
-            slug = slugify(title)
-            slug_dict[slug] = title
 
         if os.path.isdir(path):
             files = glob(os.path.join(path, "*.ipynb"))
@@ -30,33 +26,25 @@ def mdstrip(paths):
             files = [path]
             
         for in_file in files:
-            # print ("processing file:", in_file)
-            input_nb = nbf.read(open(in_file), "ipynb")
-            worksheet = input_nb.worksheets[0]
             input_nb_name = basename(in_file)
             slug = input_nb_name[:-6]
-            if slug not in slug_dict:
-                print("Missing slug:", slug)
-                title = slug
-            else:
-                title = slug_dict[slug]
-            source_lines = ["""## <img src="https://dl.dropboxusercontent.com/u/6117375/"""
-                            """intermediate-notebooks/title_graphic.png" /> {}\n""".format(title)]
-            cell_list = [nbf.new_text_cell(cell_type="markdown", source=source_lines)]
+            title = slug.replace("_", " ")
+            actual_title_nb = io.StringIO(
+                    title_data.replace("{{ title }}", title))
+            title_nb = nbf.read(actual_title_nb, "ipynb")
+            title_cell = title_nb.worksheets[0].cells[0]
+            input_nb = nbf.read(open(in_file), "ipynb")
+            worksheet = input_nb.worksheets[0]
             # add graphic here & append to cell_list
-
+            cell_list = [title_cell]
             for cell in worksheet.cells:
                 if cell.cell_type == ("code"):
                     cell.outputs = []
-                    #cell.outputs = [{
-                        #"metadata": {},
-                        #"output_type": "pyout",
-                        #"text": [""]
-                    #}]                                                                                                                                                                                                                    
+                    cell_list.append(cell)
+                elif cell.cell_type == "heading":
                     cell_list.append(cell)
             output_nb = nbf.new_notebook()
-            # print (os.path.abspath(os.path.join("code_nbs", input_nb_name)))
-            output_nb_name = os.path.join("code_nbs", input_nb_name)
+            output_nb_name = slug+".prod.ipynb"
             output_nb.worksheets.append(nbf.new_worksheet(cells=cell_list))
             
             with open(output_nb_name, 'w') as f:
